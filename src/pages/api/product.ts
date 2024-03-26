0fc9d27d60e9c1509dcbefd5da94c245de0fe8be
lukman2024 <rukumang.com@gmail.com>
2024-03-26 14:45:06 +0700

    mempelajari next js (API ROUTES)

--- src/pages/api/product.ts
+++ src/pages/api/product.ts
@@ -8,6 +8,6 @@ type Data = {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const data = await retrieveData("productsc");
+  const data = await retrieveData("produtcs");
   res.status(200).json({ status: true, statusCode: 200, data });
 }
