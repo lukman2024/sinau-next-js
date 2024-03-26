820ea7cd39143cce854b2b769b2fba3e316a0731
lukman2024 <rukumang.com@gmail.com>
2024-03-26 14:24:33 +0700

    mempelajari next js (eerrorgit add .)

---
+++ src/pages/api/product.ts
@@ -0,0 +1,13 @@
+import { retrieveData } from "@/lib/firebase/service";
+import { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  status: boolean;
+  statusCode: Number;
+  data: any;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const data = await retrieveData("productsc");
+  res.status(200).json({ status: true, statusCode: 200, data });
+}
