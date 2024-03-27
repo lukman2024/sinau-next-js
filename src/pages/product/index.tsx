53a7d15924902c5ef97f792ab0c6920fd05e6203
lukman2024 <rukumang.com@gmail.com>
2024-03-27 14:33:47 +0700

    mempelajari next js (client side rendering dan server -side rendering)

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,10 +1,12 @@
+import { fetcher } from "@/lib/swr/fetcher";
 import ProductView from "@/views/product";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const ProductPage = () => {
   const [isLogin, setIsLogin] = useState(true);
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
   const { push } = useRouter();
 
   useEffect(() => {
@@ -13,18 +15,19 @@ const ProductPage = () => {
     }
   }, []);
 
-  useEffect(() => {
-    fetch("/api/product")
-      .then((res) => res.json())
-      .then((response) => {
-        console.log(response.data);
-        setProducts(response.data);
-      });
-  }, []);
+  const { data, error, isLoading } = useSWR("/api/product", fetcher);
+
+  // useEffect(() => {
+  //   fetch("/api/product")
+  //     .then((res) => res.json())
+  //     .then((response) => {
+  //       setProducts(response.data);
+  //     });
+  // }, []);
 
   return (
     <div>
-      <ProductView products={products} />
+      <ProductView products={isLoading ? [] : data.data} />
     </div>
   );
 };
