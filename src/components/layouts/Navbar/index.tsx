142274e2f6fa8637224700bdebce2cb5b5a6b028
lukman2024 <rukumang.com@gmail.com>
2024-04-26 09:28:08 +0700

    mempelajari next js ( Setup Next Auth)

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -1,9 +1,23 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const { data }: any = useSession();
   return (
     <div className={styles.navbar}>
-      <div>Navbar</div>
+      <div className="big">Navbar</div>
+      <div>
+        {data && data.user.fullname}
+        {data ? (
+          <button className={styles.button} onClick={() => signOut()}>
+            Sign out
+          </button>
+        ) : (
+          <button className={styles.button} onClick={() => signIn()}>
+            Sign In
+          </button>
+        )}
+      </div>
     </div>
   );
 };
